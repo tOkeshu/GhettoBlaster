dc3c5eeb3c7f0871a5f766ed756fe8bfc3259d9c
Romain Gauthier <romain.gauthier@monkeypatch.me>
2014-10-22 23:09:15 +0200

    Implement the Importer component

--- client/views/tracks.jsx
+++ client/views/tracks.jsx
@@ -4,6 +4,34 @@ module("views/tracks", function(require) {
   var MusicTrack     = require("models/tracks").Track;
   var FileListReader = require("lib/file-list-reader");
 
+  var Importer = React.createClass({
+    onFiles: function(event) {
+      var reader = new FileListReader(event.target.files);
+      reader.filterBy('audio/mpeg').forEach(function(file) {
+        reader.read(file, function(blob) {
+          var track = new MusicTrack(blob);
+          this.props.tracks.add(track);
+        }.bind(this));
+      }.bind(this));
+    },
+
+    selectFile: function(event) {
+      event.preventDefault();
+      this.getDOMNode().querySelector("input[type=file]").click();
+    },
+
+    render: function() {
+      return (
+        <li className="importer">
+          <input type="file" multiple style={{display: "none"}} onChange={this.onFiles}/>
+          <a href="#" onClick={this.selectFile}>
+            <p>Import Tracks</p>
+          </a>
+        </li>
+      );
+    }
+  });
+
   var Track = React.createClass({
     addToQueue: function() {
       this.props.queue.add(this.props.track);
@@ -40,6 +68,7 @@ module("views/tracks", function(require) {
       return (
         <section className={className} data-type="list" data-position="left">
           <ul>
+            <Importer tracks={this.props.tracks}/>
             {this.state.tracks.map(function(track) {
               return <Track track={track} queue={queue}/>;
             }.bind(this))}
