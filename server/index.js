ac280317746771cfee704ca1d2a870ec7ddc1378
Romain Gauthier <romain.gauthier@monkeypatch.me>
2014-10-22 23:18:01 +0200

    Implement a simple server on top of Smoke-Signals
    For now it only serves static files from build/

---
+++ server/index.js
@@ -0,0 +1,18 @@
+var inherits = require("util").inherits;
+var SmokeServer = require("smoke-signals").SmokeServer;
+var express = require("express");
+
+function GhettoBlasterServer(config) {
+  SmokeServer.call(this, config);
+
+  var OPTIONS = {root: __dirname + "/../"};
+  this.app.use('/', express.static(__dirname + '/../build'));
+  this.app.get("/", function(req, res) {
+    res.sendfile('/client/index.html', OPTIONS);
+  });
+}
+
+inherits(GhettoBlasterServer, SmokeServer);
+
+module.exports = GhettoBlasterServer;
+
