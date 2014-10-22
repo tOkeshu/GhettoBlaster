2aad2ceab3753dc23c2a529752d334fa5cadecd1
Romain Gauthier <romain.gauthier@monkeypatch.me>
2014-10-22 23:08:10 +0200

    Glue everything together into main.js

---
+++ client/main.js
@@ -0,0 +1,13 @@
+(function() {
+  var Player    = require("lib/player");
+  var App       = require("views/app");
+  var TrackList = require("models/tracks").TrackList;
+  var Queue     = require("models/queue");
+
+  var deps = {
+    player: new Player(),
+    queue:  new Queue(),
+    tracks: new TrackList()
+  };
+  React.renderComponent(App(deps), document.querySelector('body'));
+}())
