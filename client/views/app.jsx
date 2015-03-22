af39a4c16e6e1a93cfb33343eefa5eca6aed204f
Romain Gauthier <romain.gauthier@monkeypatch.me>
2015-03-22 22:30:03 +0100

    Clean up the App view

--- client/views/app.jsx
+++ client/views/app.jsx
@@ -1,19 +1,12 @@
 /** @jsx React.DOM */
 
 define(function(require, exports, module) {
-  var Tracks = require("views/tracks").TrackList;
-  var Queue  = require("views/queue").Queue;
   var Albums = require("views/albums").Albums;
   var Album  = require("views/albums").Album;
-
-  var stateTree = require("flux/state");
-  var Dispatcher = require("flux/dispatcher");
-  var dispatcherMixin = Dispatcher.mixin(stateTree);
+  var Tracks = require("views/tracks").TrackList;
+  var Queue  = require("views/queue").Queue;
 
   var App = React.createClass({
-    mixins: [stateTree.mixin],
-    cursor: ['panel'],
-
     render: function() {
       return (
         <article>
